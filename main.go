07f2c45ab589678d4791dba567a6c82f609d2fa3
沐婉悠 <muwanyou@outlook.com>
2025-06-24 20:27:19 +0800

    修改名称

--- main.go
+++ main.go
@@ -1,15 +1,5 @@
 package main
 
-import (
-	"context"
-)
-
 func main() {
-	srv := NewServer(
-		Address("0.0.0.0:6021"),
-	)
-	RegisterReadHandler(srv, func(sessionID string, bytes []byte) error {
-		return nil
-	})
-	srv.Start(context.Background())
+
 }
